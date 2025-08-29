30772e0764ca16fb2035355575a18c377853ed01
KoenGreuell <koengreuell@gmail.com>
2025-08-29 17:26:11 +0200

    Revert "Add test files"
    This reverts commit bbdd2b44a011a75ed5bbfbdd4987cfff5975b1cf.

--- flavors/veluwe-forest-model/tests/test_landis.R
+++ flavors/veluwe-forest-model/tests/test_landis.R
@@ -1,3 +1,4 @@
-setwd("TestPnET_AllExtension")
-ret <- system("call landis-ii-8 scenario.txt")
+system("git clone https://github.com/LANDIS-II-Foundation/Extension-Biomass-Succession.git")
+setwd("Extension-Biomass-Succession/testings/CoreV8.0-BiomassSuccession7.0-SingleCell/")
+ret <- system("dotnet $LANDIS_CONSOLE scenario.txt")
 if (ret != 0) quit(status=ret)
