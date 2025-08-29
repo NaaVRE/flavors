bbdd2b44a011a75ed5bbfbdd4987cfff5975b1cf
KoenGreuell <koengreuell@gmail.com>
2025-08-29 14:19:32 +0200

    Add test files
    copied from https://github.com/LANDIS-II-Foundation/Tool-Docker-Apptainer

--- flavors/veluwe-forest-model/tests/test_landis.R
+++ flavors/veluwe-forest-model/tests/test_landis.R
@@ -1,4 +1,3 @@
-system("git clone https://github.com/LANDIS-II-Foundation/Extension-Biomass-Succession.git")
-setwd("Extension-Biomass-Succession/testings/CoreV8.0-BiomassSuccession7.0-SingleCell/")
-ret <- system("dotnet $LANDIS_CONSOLE scenario.txt")
+setwd("TestPnET_AllExtension")
+ret <- system("call landis-ii-8 scenario.txt")
 if (ret != 0) quit(status=ret)
