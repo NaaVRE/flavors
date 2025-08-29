5f516affc3d26482fbb262198e5113337880a69e
KoenGreuell <koengreuell@gmail.com>
2025-08-29 17:45:21 +0200

    Test on empty scenario file

--- flavors/veluwe-forest-model/tests/test_landis.R
+++ flavors/veluwe-forest-model/tests/test_landis.R
@@ -1,4 +1,6 @@
-system("git clone https://github.com/LANDIS-II-Foundation/Extension-Biomass-Succession.git")
-setwd("Extension-Biomass-Succession/testings/CoreV8.0-BiomassSuccession7.0-SingleCell/")
-ret <- system("dotnet $LANDIS_CONSOLE scenario.txt")
-if (ret != 0) quit(status=ret)
+file.create("empty_file.txt")
+ret <- system("dotnet $LANDIS_CONSOLE empty_file.txt", intern = TRUE)
+# Check that the return code indicates that the scenario file is invalid
+if (!grepl("Expected the name", ret[5])) {
+  quit(status = ret)
+}
