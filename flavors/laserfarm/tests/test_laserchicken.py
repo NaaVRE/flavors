b39d78f2ca27ffc126760dc060af5fd8e5482dd1
skoulouzis <koulouzis@gmail.com>
2025-10-09 13:22:07 +0200

    added test for AHN5

--- flavors/laserfarm/tests/test_laserchicken.py
+++ flavors/laserfarm/tests/test_laserchicken.py
@@ -8,3 +8,20 @@ from laserfarm import Retiler, DataProcessing, GeotiffWriter, MacroPipeline
 from laserfarm.remote_utils import get_wdclient, get_info_remote, list_remote
 from laserfarm.data_processing import DataProcessing
 from laserchicken.compute_neighbors import compute_neighborhoods
+
+
+from laserchicken import load
+# Download a sample point cloud from https://basisdata.nl/hwh-ahn/AHN5/01_LAZ/2023_C_25FN2.LAZ
+import requests
+
+url = "https://basisdata.nl/hwh-ahn/AHN5/01_LAZ/2023_C_25FN2.LAZ"
+output_path = "2023_C_25FN2.LAZ"
+
+response = requests.get(url, stream=True)
+response.raise_for_status()
+with open(output_path, "wb") as f:
+    for chunk in response.iter_content(chunk_size=8192):
+        f.write(chunk)
+print(f"Downloaded to {output_path}")
+
+point_cloud = load(output_path)
