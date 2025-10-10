9c9383fdb7ca52ab09c41c816e99b185420da3c7
skoulouzis <koulouzis@gmail.com>
2025-10-10 11:35:13 +0200

    set correct merge

--- flavors/mercury/tests/tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-# activate environment in cell-test or in jupyter image
-source /venv/bin/activate || eval "$(conda shell.bash activate mercury)"
-dir="${0%/*}"
-find "$dir" -name "*.py" -print0 | xargs --null -I "{}" python "{}"
