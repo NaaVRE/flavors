0df383edf697be2641803a91a6b5182346905bef
skoulouzis <koulouzis@gmail.com>
2025-10-10 11:37:27 +0200

    dont test for AHN5

--- flavors/laserfarm/tests/test_laserchicken.py
+++ flavors/laserfarm/tests/test_laserchicken.py
@@ -34,9 +34,9 @@ output_path = "C_20BN2.LAZ"
 
 download_file_and_load(url, output_path)
 
-# AHN5
-url = "https://basisdata.nl/hwh-ahn/AHN5/01_LAZ/2023_C_20BN2.LAZ"
-output_path = "2023_C_20BN2.LAZ"
-
-
-download_file_and_load(url, output_path)
+# # AHN5
+# url = "https://basisdata.nl/hwh-ahn/AHN5/01_LAZ/2023_C_20BN2.LAZ"
+# output_path = "2023_C_20BN2.LAZ"
+#
+#
+# download_file_and_load(url, output_path)
