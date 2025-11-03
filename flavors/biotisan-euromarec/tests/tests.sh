6c8d8eda8782323477f56b37f64ae9f9f40974ef
KoenGreuell <koengreuell@gmail.com>
2025-11-03 17:21:41 +0100

    Add run workflow in R test

--- flavors/biotisan-euromarec/tests/tests.sh
+++ flavors/biotisan-euromarec/tests/tests.sh
@@ -3,5 +3,6 @@ set -e
 # activate environment in cell-test or in jupyter image
 source /venv/bin/activate || eval "$(conda shell.bash activate biotisan-euromarec)"
 dir="${0%/*}"
+cd "$dir"
 find "$dir" -name "*.py" -print0 | xargs --null -I "{}" python "{}"
 find "$dir" -name "*.R" -print0 | xargs --null -I "{}" Rscript "{}"
