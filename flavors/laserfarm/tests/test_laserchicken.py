440609cf8b9e3d5d30263eea397337ad611891bb
skoulouzis <koulouzis@gmail.com>
2025-11-03 16:57:16 +0100

    https://basisdata.nl/hwh-ahn/ahn4/01_LAZ/C_20BN2.LAZ

--- flavors/laserfarm/tests/test_laserchicken.py
+++ flavors/laserfarm/tests/test_laserchicken.py
@@ -29,7 +29,7 @@ def download_file_and_load(url, output_path):
 # https://geotiles.citg.tudelft.nl/
 
 # AHN4
-url = "https://scruffy.lab.uvalight.net:9000/naa-vre-user-data/shiyifang529%40gmail.com/vl-laserfarm/AHN5/AHN5_small.laz?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=7MP8OH1CWJEZPRD71QSD%2F20251029%2Fnl-uvalight%2Fs3%2Faws4_request&X-Amz-Date=20251029T130311Z&X-Amz-Expires=43200&X-Amz-Security-Token=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NLZXkiOiI3TVA4T0gxQ1dKRVpQUkQ3MVFTRCIsImV4cCI6MTc2MTc4NjEwNywicGFyZW50Ijoic3Bpcm9zIn0.uxJi-kORKMwHFxE_BBunKQ3oyV7W3vpR14_CDRfMGY0stPtRzCOb4eNw0NQsUdLLwd2fatyViA9lhQrfNhGLVA&X-Amz-SignedHeaders=host&versionId=a2563029-4760-499e-8f11-cc1ac14a041e&X-Amz-Signature=f340f2803c5f3ebe21ec6c2439699c9afabfae7f0713f5c8db47dd4603c27e60"
+url = "https://basisdata.nl/hwh-ahn/ahn4/01_LAZ/C_20BN2.LAZ"
 output_path = "C_20BN2.LAZ"
 
 download_file_and_load(url, output_path)
