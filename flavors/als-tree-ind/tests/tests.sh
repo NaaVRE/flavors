bc107af338eea68159d32ea2067cee7c33efbc4f
KoenGreuell <koengreuell@gmail.com>
2025-11-11 14:27:02 +0100

    Test if modules are executable

--- flavors/als-tree-ind/tests/tests.sh
+++ flavors/als-tree-ind/tests/tests.sh
@@ -5,3 +5,7 @@ source /venv/bin/activate || eval "$(conda shell.bash activate vanilla)"
 dir="${0%/*}"
 find "$dir" -name "*.py" -print0 | xargs --null -I "{}" python "{}"
 find "$dir" -name "*.R" -print0 | xargs --null -I "{}" Rscript "{}"
+
+if [ -x /bin/Workflow_ALS_Trees/clipping/release/bin/ClipLas ]; then echo "ClipLas executable" ; else exit 255;  fi
+if [ -x /bin/Workflow_ALS_Trees/retile_by_count/release/bin/RetileByCount ]; then echo "RetileByCount executable" ; else exit 255;  fi
+if [ -x /bin/Workflow_ALS_Trees/retile_by_size/release/bin/RetileBySize ]; then echo "RetileBySize executable" ; else exit 255;  fi
