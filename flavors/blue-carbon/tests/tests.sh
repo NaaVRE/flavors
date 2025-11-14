5ea9c5be806aed99f3196c54f3f41602d551cb02
Quan Pan <quanpan302@hotmail.com>
2025-11-14 09:13:21 +0100

    Add blue-carbon flavor, MacroBolo project

---
+++ flavors/blue-carbon/tests/tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+# activate environment in cell-test or in jupyter image
+source /venv/bin/activate || eval "$(conda shell.bash activate quadfavl)"
+dir="${0%/*}"
+find "$dir" -name "*.py" -print0 | xargs --null -I "{}" python "{}"
+find "$dir" -name "*.R" -print0 | xargs --null -I "{}" Rscript "{}"
