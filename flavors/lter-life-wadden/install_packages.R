15a1da06da496aca3d463eb15c6b5d8b8043a48c
KoenGreuell <44636560+KoenGreuell@users.noreply.github.com>
2025-12-15 08:42:14 +0100

    Update waddenzee flavor to last commit of TempSED

--- flavors/lter-life-wadden/install_packages.R
+++ flavors/lter-life-wadden/install_packages.R
@@ -4,7 +4,7 @@ library('tools4watlas')
 devtools::install_github('allertbijleveld/SIBES@24d3f06418a8aca77db4e7fbef66c45dc55873f8', dependencies = FALSE)
 library('SIBES')
 
-devtools::install_github('TempSED/TempSED@79ecf0e2f9473c3e5e35cf53bc87d15389f2e68c', depend = TRUE)
+devtools::install_github('TempSED/TempSED@1a29a181b660ac6b2e3f038fcdc1c659e0f0962c', depend = TRUE)
 require('TempSED')
 
 devtools::install_github('leonawicz/mapmate@7c36274078c4b6ffbd9dcc278f3176b7535ba86f', dependencies = FALSE)
