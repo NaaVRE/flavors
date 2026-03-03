2203fc6cf6e0fa5c08a761373b729d25a832ea7c
Quan Pan <quanpan302@hotmail.com>
2026-03-03 14:43:33 +0100

    fix bug

--- flavors/ecvs/tests/tests.sh
+++ flavors/ecvs/tests/tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # activate environment in cell-test or in jupyter image
-source /venv/bin/activate || eval "$(conda shell.bash activate vanilla)"
+source /venv/bin/activate || eval "$(conda shell.bash activate ecvs)"
 dir="${0%/*}"
 find "$dir" -name "*.py" -print0 | xargs --null -I "{}" python "{}"
-# find "$dir" -name "*.R" -print0 | xargs --null -I "{}" Rscript "{}"
+find "$dir" -name "*.R" -print0 | xargs --null -I "{}" Rscript "{}"
