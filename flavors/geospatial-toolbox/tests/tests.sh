ed3d5ceb113c00969afe35f51018366f4c52daa0
KoenGreuell <44636560+KoenGreuell@users.noreply.github.com>
2026-03-23 09:21:29 +0100

    Rename to geospatial-toolbox

--- flavors/geospatial-toolbox/tests/tests.sh
+++ flavors/geospatial-toolbox/tests/tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # activate environment in cell-test or in jupyter image
-source /venv/bin/activate || eval "$(conda shell.bash activate homeros-seismology)"
+source /venv/bin/activate || eval "$(conda shell.bash activate geospatial-toolbox)"
 dir="${0%/*}"
 find "$dir" -name "*.py" -print0 | xargs --null -I "{}" python "{}"
 find "$dir" -name "*.R" -print0 | xargs --null -I "{}" Rscript "{}"
