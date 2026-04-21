3aca55572e24f3b8c753b11568bff9e92d9c2816
KoenGreuell <44636560+KoenGreuell@users.noreply.github.com>
2026-04-21 16:42:52 +0200

    Add bgis-urban-planning flavor

---
+++ flavors/bgis-urban-planning/tests/tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+# activate environment in cell-test or in jupyter image
+source /venv/bin/activate || eval "$(conda shell.bash activate bgis-urban-planning)"
+dir="${0%/*}"
+cd "$dir"
+find "$dir" -name "*.py" -print0 | xargs --null -I "{}" python "{}"
+find "$dir" -name "*.R" -print0 | xargs --null -I "{}" Rscript "{}"
