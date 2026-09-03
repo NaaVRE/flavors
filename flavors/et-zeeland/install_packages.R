4cc44a4d9a7963336a2ac814dc6185495862fb9f
KoenGreuell <44636560+KoenGreuell@users.noreply.github.com>
2026-09-03 13:29:59 +0200

    Test retry_requests and pyrealm

--- flavors/et-zeeland/install_packages.R
+++ flavors/et-zeeland/install_packages.R
@@ -2,4 +2,4 @@ pak::pkg_install("ReacTran")
 library("ReacTran")
 
 devtools::install_github('TempSED/TempSED@6101ced7f79b300f19999f61f50fefe6099aab76', build_vignettes = TRUE, dependencies = FALSE)
-require('TempSED')
+library('TempSED')
